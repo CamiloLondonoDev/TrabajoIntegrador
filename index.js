c7f5da67b182f4a35f2811c3f6bd4b4c3734c9bf
Sebastian Redruello <misdesarrollos2023@gmail.com>
2023-04-13 00:47:02 -0300

    Correciones 13/4
    Se eliminaron los action de los form en HTML a pedido del profe porque esto generaba errores. Se monto parte de la API, Se sumo Jquery a los head de HTML y se logro esconder los contenedores del wizard.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+// Llamo por jquery a todo el documento y escondo el 2do y 3er paso una vez que finalizo la carga
 
+$(document).ready(function(){
+    $("#firstStep").show();
+    $("#secondStep").hide();
+    $("#thirdStep").hide();
+})
 
 function wizardFirst() {
     let nombreEmpresa =  document.getElementById("nombreEmpresa").value;
     let rubroEmpresa = document.getElementById("rubroEmpresa").value;
-    let primerPaso = document.getElementById("firstStep").style.display = "none";
-    let tercerPaso = document.getElementById("thirdStep").style.display = "none";
+    $("#firstStep").hide();
+    $("#secondStep").show();
+    $("#thirdStep").hide();
 
     console.log(nombreEmpresa);
     console.log(rubroEmpresa);
@@ -15,16 +22,39 @@ function wizardSecond() {
     let apellidoPersona = document.getElementById("apellidoPersona").value;
     let telefonoPersona = document.getElementById("telefonoPersona").value;
     let mailPersona = document.getElementById("mailPersona").value;
-    let primerPaso = document.getElementById("firstStep").style.display = "none";
-    let segundoPaso = document.getElementById("secondStep").style.display = "none";
-    let tercerPaso = document.getElementById("thirdStep").style.display = ""
+
+    $("#firstStep").hide();
+    $("#secondStep").hide();
+    $("#thirdStep").show();
+
+
     console.log(nombrePersona);
+    console.log(apellidoPersona);
     console.log(telefonoPersona);
     console.log(mailPersona);
 
 
+    // Verificando los campos de la API, la conexion ya funciona y responde en parte
+    $.ajax({
+        method:"POST",
+        url: "https://reqres.in/api/users",
+        data: {
+            "name": "pepe",
+            "job": "caramelos",
+            // "nombrePersona" : nombrePersona,
+            // "apellidoPersona": apellidoPersona,
+            // "telefonoPersona": telefonoPersona,
+            // "mailPersona": mailPersona,
+    
+        }
+    }).done()
 
-    document.getElementById("thirdStep").innerHTML = nombreEmpresa
 
  
 }
+
+
+// Falta armar en jquery el validador de los campos
+// Falta meter los valores de los campos en el HTML segun el paso 3
+
+
