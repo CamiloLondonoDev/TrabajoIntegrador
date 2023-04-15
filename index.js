cd87dccaa7d5d09331954aed6b1f25799ac23c33
CamiloLondonoDev <camilo.londono.dev@gmail.com>
2023-04-14 21:12:40 -0300

    campos validados

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+                // Do not submit the form here, handle it with JavaScript instead
+                event.preventDefault();
+            }, false)
+        })
+})()
 
 // Llamo por jquery a todo el documento y escondo el 2do y 3er paso una vez que finalizo la carga
 
@@ -12,47 +33,52 @@ function wizardFirst() {
     let nombreEmpresa =  document.getElementById("nombreEmpresa").value;
     let rubroEmpresa = document.getElementById("rubroEmpresa").value;
 
-    if (nombreEmpresa.length <= 0 || nombreEmpresa == null) {
-        $("#errorNombre").text("Este campo es requerido");
-        $("#errorNombre").show();
+    if (nombreEmpresa.length == 0 || rubroEmpresa.length == 0) {
 
-    }
-    if (rubroEmpresa.length <= 0) {
-        $("#errorRubro").text("Este campo es requerido");
-        $("#errorRubro").show();
-    }
-    else{
+    } else {
         $("#firstStep").hide();
         $("#secondStep").show();
         $("#thirdStep").hide();
-
+    
         console.log(nombreEmpresa);
         console.log(rubroEmpresa);
+
     }
+
+
 }
 
 function wizardSecond() {
+   
     let nombrePersona =  document.getElementById("nombrePersona").value;
     let apellidoPersona = document.getElementById("apellidoPersona").value;
     let telefonoPersona = document.getElementById("telefonoPersona").value;
     let mailPersona = document.getElementById("mailPersona").value;
 
-    $("#firstStep").hide();
-    $("#secondStep").hide();
-    $("#thirdStep").show();
+    
+    if (nombrePersona.length == 0) {
+        
+    } else {
+        $("#firstStep").hide();
+        $("#secondStep").hide();
+        $("#thirdStep").show();
+        console.log(nombrePersona);
+        console.log(apellidoPersona);
+        console.log(telefonoPersona);
+        console.log(mailPersona);
+    
+        $("#nombreEmpresaOut").html(nombreEmpresa);
+        $("#rubroEmpresaOut").html(rubroEmpresa);
+        $("#nombrePersonaOut").html(nombrePersona);
+        $("#apellidoPersonaOut").html(apellidoPersona);
+        $("#telefonoPersonaOut").html(telefonoPersona);
+        $("#mailPersonaOut").html(mailPersona);
+    } 
 
 
-    console.log(nombrePersona);
-    console.log(apellidoPersona);
-    console.log(telefonoPersona);
-    console.log(mailPersona);
 
-    $("#nombreEmpresaOut").html(nombreEmpresa);
-    $("#rubroEmpresaOut").html(rubroEmpresa);
-    $("#nombrePersonaOut").html(nombrePersona);
-    $("#apellidoPersonaOut").html(apellidoPersona);
-    $("#telefonoPersonaOut").html(telefonoPersona);
-    $("#mailPersonaOut").html(mailPersona);
+
+
 
   
 
@@ -94,3 +120,4 @@ function inicio() {
     $("#secondStep").hide();
     $("#thirdStep").hide();
 }
+
